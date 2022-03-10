0176e03255e01838a3f3b1b4c2b94d6f2b70e81a
Brandon Cassidy <brandoncassidy323@gmail.com>
2022-03-10 16:58:14 -0500

    Add project files.

---
+++ FileImportTool/FilePrep.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileImportTool
+{
+    class FilePrep
+    {
+        //List<string[]>
+         
+        public List<string> Columns = new List<string>();
+        public List<string[]> Values = new List<string[]>();
+        /************************
+         Created: 07/23/21
+         Last Updated: 07/23/21
+         Description: takes file passed in at runtime, reads in it contents into an object that has the name/value pairs 
+         Params: Object? - Do we need to pass in, or create and return?
+                 File - The delimited file to be worked on.
+                 Delimeter - The character that denotes the separation of columns. typically comma.
+         Returns: SqlConnection object.
+        ***********************/
+        public void LoadFile(string filePath, string fileExtension, string delimeter) {
+            //[TODO: Is there a more efficient way to check the type of file? and for CSV, can we discover the delimeter instead of user input?]
+            switch (fileExtension)
+            {
+                case ".csv":
+                    ImportCsv(filePath, delimeter);
+                    break;
+                case ".xslx":
+                    ImportExcel(filePath);
+                    break;
+            }
+        }
+        /************************
+         Created: 07/23/21
+         Last Updated: 07/23/21
+         Description: Reads file in and loads data into a list.
+         Params: FilePath
+                 Delimeter - The character that denotes the separation of columns. typically comma.
+         Returns: SqlConnection object.
+        ***********************/
+        private void ImportExcel(string filePath)
+        {
+            throw new NotImplementedException();
+        }
+        /************************
+         Created: 07/23/21
+         Last Updated: 07/23/21
+         Description: takes file passed in at runtime, reads in it contents into a two objects that store 
+                      the column names and data records from the file. 
+         Params: File - The delimited file to be worked on.
+                 Delimeter - The character that denotes the separation of columns. typically comma.
+         Returns: N/A
+        ***********************/
+        private void ImportCsv(string filePath, string delimeter)
+        {
+            List<string[]> csv = new List<string[]>();
+            var lines = File.ReadAllLines(filePath);
+
+            //Put the column names into columns list.
+            foreach (string value in lines[0].Split(delimeter.ToCharArray()))
+            {
+                Columns.Add(value);
+            }
+
+            //Insert records into the Values list.
+            for(int i = 1; i < lines.Count(); i++)
+            {
+                Values.Add(lines[i].Split(delimeter.ToCharArray()));
+            }
+
+
+        }    
+    }
+}
+
+    
+
---
+++ FileImportTool/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+
+namespace FileImportTool
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            // Code to return to the SQL job agent. 0 = success, negative = failure. We can customize this if we think it out.
+            int returnCode = 0;
+
+            // bools to help determine which actions to take. Popul
+            bool tableExists = false;
+            bool columnsMatch = true; //testing
+            bool tableCreated = false;
+            bool truncateTable = false;
+
+
+            // Set params sent in at runtime to be used in the program.
+            // Param order: // [0]-FilePath | [1]-TableName | [2] TruncateYesNo | [3]-Delimeter.
+            string filePath = args[0];//Environment.GetCommandLineArgs()[0];
+            string tableString = args[1];//Environment.GetCommandLineArgs()[1];
+
+            // Set the Truncate table variable to true if the value is yes. Defaulted to no on initialization.
+            if (args[2].ToLower() == "yes")
+            {
+                truncateTable = true;
+            }
+            
+            // Assign the delimeter of the file if CSV.
+            string delimeter = args[3];
+
+            // Get the extension of the file to decide how to import the file.
+            string fileExtension = Path.GetExtension(filePath);
+            
+            //Breaks table name into its parts to be used in the connection string.
+            List<string> tableComponents = new List<string>();
+            foreach (string value in tableString.Split('.'))
+            {
+                tableComponents.Add(value);// [0]-Server | [1]-Database | [2] Schema | [3]-Table name.
+            }
+            
+            string server = tableComponents[0].ToString();
+            string database = tableComponents[1].ToString();
+            //Builds the schema.table name structure that SQL expects. 2 = schema, 3 = table name.
+            string tableName = $"{tableComponents[2].ToString()}.{tableComponents[3].ToString()}";
+
+            // Set up connection string. [TODO: Could this be generalized further to allow it to work outside of SHU?]
+            string connString = $"Data Source={server}.sacredheart.edu;Initial Catalog={database};Integrated Security=SSPI";
+
+            //Create objects needed.
+            FilePrep fp = new FilePrep();
+            Sql sql = new Sql();
+
+            // Import the file.
+            fp.LoadFile(filePath, fileExtension, delimeter);
+
+            // Create Sql connection. Future proofing in case we go multi-server in future release.
+            sql.ConnectToSql(connString);
+
+            // See if table exists.
+            tableExists = sql.CheckTable(tableName);
+
+            // If table exists, check whether or not to truncate the table.
+            if (tableExists && truncateTable)
+            {
+                sql.TruncateTable(tableName);
+            }
+
+            //// If table exists, check whether or not the columns match.
+            //if (tableExists)
+            //{
+            //    columnsMatch = sql.CheckColumns(tableName, fp.Columns);
+            //}
+
+            // If table does not exist, create it set tableCreated to true.
+            if (!tableExists)
+            {
+                tableCreated = sql.CreateTable(tableName, fp.Columns);
+
+            }
+
+            // If table exists and the columns match or the table was created then insert the rows.
+            if ((tableExists && columnsMatch) || tableCreated)
+            {
+                sql.InsertRows(tableName, fp.Columns, fp.Values);
+            }
+
+
+            //Test output
+            //Console.WriteLine($"truncateTable = {truncateTable}");
+            //Console.WriteLine($"tableExists = {tableExists}");
+            //Console.WriteLine($"columnsMatch = {columnsMatch}");
+            //Console.WriteLine($"tableCreated = {tableCreated}");
+
+            //Console.ReadLine();
+
+            sql.CloseConnection();
+            // Return the status code to indicate success or failure.
+            return returnCode;
+        }
+    }
+}
